1788d77a6dc5746c99cecb6442ad2930e2d21be8
潘强 <1126525692@qq.com>
2020-08-06 23:55:06 +0800

    加入 sentry依赖

---
+++ app/src/main/java/com/how/to/use/sentry/App.kt
@@ -0,0 +1,11 @@
+package com.how.to.use.sentry
+
+import android.app.Application
+import io.sentry.core.Sentry
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
--- app/src/main/java/com/how/to/use/sentry/MainActivity.kt
+++ app/src/main/java/com/how/to/use/sentry/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.how.to.use.sentry
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.sentry.core.Sentry
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            try {
+                throw Exception("This is a test.")
+            } catch (e: Exception) {
+                Sentry.captureException(e)
+            }
+        }
+
     }
 }
